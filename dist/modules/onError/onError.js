4751652722be349f562eb2cc51a8c4a0f77133dd
Глеб <gleblubiskin@gmail.com>
2022-06-24 08:17:31 +0300

    Исправление бага с портом в конфиге

--- dist/modules/onError/onError.js
+++ dist/modules/onError/onError.js
@@ -8,34 +8,33 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * Module dependencies.
  */
-const express_1 = __importDefault(require("express"));
+const express_1 = __importDefault(r'express'xpress"));
 const normalizePort_1 = __importDefault(
-  require("../normalizePort/normalizePort")
-);
-const config_1 = __importDefault(require("config"));
+    r'../normalizePort/normalizePort'zePort"));
+const config_1 = __importDefault(r'config'config"));
 // App
 const app = (0, express_1.default)();
 /**
  * Get port from environment and store in Express.
  */
 const port = (0, normalizePort_1.default)(
-  process.env.PORT || config_1.default.get("port")
-);
+    process.env.PORT || config_1.default.get('port'));
 app.set("port", port);
 /**
  * Event listener for HTTP server "error" event.
  */
 const onError = (error) => {
-  if (error.syscall !== "listen") throw error;
-  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+  if (error.syscall !== 'listen')
+    throw error;
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
       process.exit(1);
       break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
       process.exit(1);
       break;
     default:
