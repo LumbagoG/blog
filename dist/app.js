807f50d63d6fee932f1afba6af84b6d647f4a3df
Глеб <gleblubiskin@gmail.com>
2022-06-24 09:59:17 +0300

    Перевисывание роутера на typescript

--- dist/app.js
+++ dist/app.js
@@ -1,26 +1,32 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * Module dependencies.
  */
 const express_1 = __importDefault(require("express"));
-const dbConnection_1 = __importDefault(require("./modules/dbConnection/dbConnection"));
-const serverConnection_1 = __importDefault(require("./modules/serverConnection/serverConnection"));
+const dbConnection_1 = __importDefault(
+  require("./modules/dbConnection/dbConnection")
+);
+const serverConnection_1 = __importDefault(
+  require("./modules/serverConnection/serverConnection")
+);
 // Imports routes
-const index_1 = __importDefault(require('./routes/index'));
+const index_1 = __importDefault(require("./routes/index"));
 // App
 const app = (0, express_1.default)();
 // Connect to server
 serverConnection_1.default
-    .then(() => console.log("Server ok"))
-    .catch((err) => console.log(`Error server: ${err}`));
+  .then(() => console.log("Server ok"))
+  .catch((err) => console.log(`Error server: ${err}`));
 // Connect to MongoDB
 dbConnection_1.default
-    .then(() => console.log("DB OK"))
-    .catch((err) => console.log(`DB FAIL: ${err}`));
+  .then(() => console.log("DB OK"))
+  .catch((err) => console.log(`DB FAIL: ${err}`));
 // Routes
 app.use("/", index_1.default);
 //# sourceMappingURL=app.js.map
--- routes/index.ts
+++ routes/index.ts
@@ -5,10 +5,10 @@ const router = express.Router();
 
 /* GET home page. */
 router.get(
-    "/",
-    function (req: any, res: { send: (arg0: string) => void }, next: any) {
-        res.send("Index");
-    }
+  "/",
+  function (req: any, res: { send: (arg0: string) => void }, next: any) {
+    res.send("Index");
+  }
 );
 
 // Export
