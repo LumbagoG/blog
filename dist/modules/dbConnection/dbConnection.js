c05e29eb827f878a4d34aaae51ab746fe0fcdc8d
Глеб <gleblubiskin@gmail.com>
2022-06-25 12:49:40 +0300

    Исправил баг с devDependencies

--- dist/modules/dbConnection/dbConnection.js
+++ dist/modules/dbConnection/dbConnection.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -21,8 +12,8 @@ const config_1 = __importDefault(require("config"));
  * Create connection to MongoDB
  * @returns {Promise<void>}
  */
-const dbConnection = () => __awaiter(void 0, void 0, void 0, function* () {
-    yield mongoose_1.default.connect(config_1.default.get("mongoUri"));
-});
+const dbConnection = async () => {
+  await mongoose_1.default.connect(config_1.default.get("mongoUri"));
+};
 exports.default = dbConnection();
 //# sourceMappingURL=dbConnection.js.map
--- dist/modules/serverConnection/serverConnection.js
+++ dist/modules/serverConnection/serverConnection.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -35,15 +26,14 @@ const server = http_1.default.createServer(app);
  * Create connect to server
  * @returns {Promise<void>}
  */
-const serverConnection = () => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        server.listen(port, () => console.log(`Server started on port ${port}`));
-        server.on("listening", onListening_1.default);
-        server.on("error", onError_1.default);
-    }
-    catch (e) {
-        console.log(`Error: ${e}`);
-    }
-});
+const serverConnection = async () => {
+  try {
+    server.listen(port, () => console.log(`Server started on port ${port}`));
+    server.on("listening", onListening_1.default);
+    server.on("error", onError_1.default);
+  } catch (e) {
+    console.log(`Error: ${e}`);
+  }
+};
 exports.default = serverConnection();
 //# sourceMappingURL=serverConnection.js.map
