73280a7060755f21be6b18ae2e2dbb487085c445
Глеб <gleblubiskin@gmail.com>
2022-07-11 08:33:51 +0300

    Создание проекта, настройка eslint и prettier

---
+++ src/pages/app/app.jsx
@@ -0,0 +1,18 @@
+// Modules
+import React from 'react';
+
+// Styles
+import './app.scss';
+
+/**
+ * App
+ * @return {JSX.Element}
+ * @constructor
+ */
+export const App = () => {
+    return (
+        <>
+            <h1>Hello</h1>
+        </>
+    );
+};
